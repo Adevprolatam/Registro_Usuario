d1dc573e471e892da62340829af203b1559282ff
Adrian <adevprolatam@gmail.com>
2024-02-07 16:21:31 -0500

    Proyect

---
+++ src/main/java/adevprolatam/registro_usuario/Registro_Usuario.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package adevprolatam.registro_usuario;
+import Frontend.LoginJFrame;
+/**
+ *
+ * @author dell
+ */
+public class Registro_Usuario {
+
+    public static void main(String[] args) {
+      LoginJFrame view = new LoginJFrame();
+      view.setVisible(true);
+      view.setLocationRelativeTo(null);
+    }
+}
