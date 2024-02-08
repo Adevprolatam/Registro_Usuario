769ac9c19c14a382d072757837d63d9b6ba6cafb
Adrian <adevprolatam@gmail.com>
2024-02-07 19:46:34 -0500

    Backend

---
+++ src/main/java/Backend/Usuario.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+import org.apache.commons.codec.digest.DigestUtils;
+/**
+ *
+ * @author dell
+ */
+public class Usuario {
+    private String nombre;
+    private String email;
+    private String password;
+
+    public Usuario(String nombre, String email, String password) {
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+    }
+    
+    // @GETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // @SETTERS
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public void encriptarPassword() {
+        this.password = DigestUtils.sha256Hex(this.password);
+    }
+    
+}
--- src/main/java/adevprolatam/registro_usuario/Registro_Usuario.java
+++ src/main/java/adevprolatam/registro_usuario/Registro_Usuario.java
@@ -3,7 +3,10 @@
  */
 
 package adevprolatam.registro_usuario;
+//IMPORTS
 import Frontend.LoginJFrame;
+import Backend.RegistroUsuario;
+import Backend.Usuario;
 /**
  *
  * @author dell
@@ -11,8 +14,22 @@ import Frontend.LoginJFrame;
 public class Registro_Usuario {
 
     public static void main(String[] args) {
-      LoginJFrame view = new LoginJFrame();
-      view.setVisible(true);
-      view.setLocationRelativeTo(null);
+        LoginJFrame view = new LoginJFrame();
+        view.setLocationRelativeTo(null);
+        view.setVisible(true);
+        //Usuario usuario1 = new Usuario("Juan", "juan@example.com", "contrasena123");
+        //Usuario usuario2 = new Usuario("María", "maria@example.com", "contrasena456");
+
+        // Registrar los usuarios 
+            //RegistroUsuario registroUsuario = new RegistroUsuario();
+            //registroUsuario.registrarUsuario(usuario1);
+            //registroUsuario.registrarUsuario(usuario2);
+
+        // Obtener todos los usuarios registrados
+        //System.out.println("Usuarios registrados:");
+        //for (Usuario usuario : registroUsuario.getUsuarios()) {
+        //    System.out.println("Nombre: " + usuario.getNombre() + ", Email: " + usuario.getEmail() + ", Contraseña: " + usuario.getPassword());
+        //}
+        
     }
 }
